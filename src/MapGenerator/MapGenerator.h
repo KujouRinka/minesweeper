ec8749030a3f4c2d12d42925baa5a74bdb6fdfa7
KujouRinka <breakhopec@gmail.com>
2021-04-05 20:01:28 +0800

    add some detail

--- src/MapGenerator/MapGenerator.h
+++ src/MapGenerator/MapGenerator.h
@@ -9,14 +9,41 @@
 #ifndef MINESWEEPER_RANDOMMINE_H
 #define MINESWEEPER_RANDOMMINE_H
 
+#include <cstdint>
+
 namespace MapGenerator {
 
-    struct MField{
-        uint16_t width;
-        uint16_t length;
+    enum LEVEL {
+        easy, normal, hard
+    };
+
+    struct MField {
+        uint16_t row;
+        uint16_t line;
         uint16_t numberOfMines;
         void *mineMap{};
+
+        MField() = default;
+
+        explicit MField(LEVEL level);
+
+        MField(uint16_t cRow, uint16_t cLine, uint16_t cMines) :
+                row(cRow), line(cLine), numberOfMines(cMines) {}
     };
+
+    MField::MField(LEVEL level) {
+        if (level == easy) {
+            this->row = this->line = 9;
+            this->numberOfMines = 10;
+        } else if (level == normal) {
+            this->row = this->line = 16;
+            this->numberOfMines = 40;
+        } else if (level == hard) {
+            this->row = 16;
+            this->line = 30;
+            this->numberOfMines = 99;
+        }
+    }
 
     typedef MField *Minefield;
 
